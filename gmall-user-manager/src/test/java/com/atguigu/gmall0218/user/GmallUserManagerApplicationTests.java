fb70441eea8f3d773b03f55a0508ac0cdc1030ab
dapan <dapan@110.com>
2021-05-14 20:27:00 +0800

    first commit

---
+++ gmall-user-manager/src/test/java/com/atguigu/gmall0218/user/GmallUserManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atguigu.gmall0218.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GmallUserManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
